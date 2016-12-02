de224212e258958bfd6d6ac7d74909dee52e3a56
Nasim Tabatabaei <nasim.taba2@gmail.com>
2016-12-02 14:02:13 -0800

    commiting

--- tests/testthat/testex5.R
+++ tests/testthat/testex5.R
@@ -1,5 +1,7 @@
 context("Temperature converter")
 
+
+# unit testing for fahr_to_kelvin function
 test_that("function returns the correct results", {
   expect_equal(fahr_to_kelvin(0), 255.37222222222)
   expect_equal(fahr_to_kelvin(10), 260.9278 + -2.22e-05)
@@ -7,7 +9,7 @@ test_that("function returns the correct results", {
 
 
 
-
+# unit testing for kelvin_to_celsius function
 test_that("function returns the correct results", {
   expect_equal(kelvin_to_celsius(0), -273.15)
   expect_equal(kelvin_to_celsius(10), -263.15)
@@ -16,25 +18,28 @@ test_that("function returns the correct results", {
 
 
 
-
+# unit testing for celsius_to_fahr function
 test_that("function returns the correct results", {
   expect_equal(celsius_to_fahr(1), 33.8)
   expect_equal(celsius_to_fahr(10), 50)
 })
 
 
+# unit testing for fahr_to_celsius function
 test_that("function returns the correct results", {
   expect_equal(fahr_to_celsius(1), -17.2222 + -2.22e-05 )
   expect_equal(fahr_to_celsius(10), -12.222222222222)
 })
 
 
+# unit testing for kelvin_to_fahr function
 test_that("function returns the correct results", {
   expect_equal(kelvin_to_fahr(1), -457.87)
   expect_equal(kelvin_to_fahr(10), -441.67)
 })
 
 
+# unit testing for celsius_to_kelvin function
 test_that("function returns the correct results", {
   expect_equal(celsius_to_kelvin(1), 274.15)
   expect_equal(celsius_to_kelvin(10), 283.15)
